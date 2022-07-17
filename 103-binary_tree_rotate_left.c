3b8f925f406a3c0a085b420b47c8b4d9fbb86d00
Nachop51 <nachoperalta0@gmail.com>
2022-07-17 22:40:15 +0000

    Fixing betty and adding task 104

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_rotate_left - Rotates a binary tree to the left
+ * @tree: Pointer to the tree
+ *
+ * Return: Pointer to the new root node
+ */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *parent;
--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_rotate_right - Rotates a binary tree to the right
+ * @tree: Pointer to the tree
+ *
+ * Return: Pointer to the new root node
+ */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
+	binary_tree_t *parent;
+
+	if (!tree || !tree->left)
+		return (tree);
+	parent = tree->left;
+	parent->parent = tree->parent;
+	if (parent->right)
+		(parent->right)->parent = tree;
+	tree->left = parent->right;
+	parent->right = tree;
+	tree->parent = parent;
+	return (parent);
 }
