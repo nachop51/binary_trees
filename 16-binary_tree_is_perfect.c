19c8c89464b3bdb3c3c35f987916e2f073db960b
Nachop51 <nachoperalta0@gmail.com>
2022-07-17 15:29:21 +0000

    Fixing typo

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,7 +29,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right = 0;
 
 	if (!tree)
-		return (-1);
+		return (0);
 	if (tree->left)
 		left = binary_tree_height(tree->left) + 1;
 	if (tree->right)
