7124e70ca17b7e4d954477a8fcadf92a21f0fbb3
Nachop51 <nachoperalta0@gmail.com>
2022-07-17 22:37:06 +0000

    Rotate left added

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -2,4 +2,16 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *parent;
+
+	if (!tree || !tree->right)
+		return (tree);
+	parent = tree->right;
+	parent->parent = tree->parent;
+	if (parent->left)
+		(parent->left)->parent = tree;
+	tree->right = parent->left;
+	parent->left = tree;
+	tree->parent = parent;
+	return (parent);
 }
