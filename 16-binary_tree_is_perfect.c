f575740d544eac36c9d94d2a900d45d039ebe059
Nachop51 <nachoperalta0@gmail.com>
2022-07-17 17:54:51 +0000

    Fixing task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,5 +47,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree || !binary_tree_is_full(tree))
 		return (0);
-	return (binary_tree_height(tree->left) == binary_tree_height(tree->right));
+	if (!tree->left && !tree->right)
+		return (1);
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	return (0);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -19,13 +19,16 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /* Functions */
 
@@ -49,5 +52,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second);
 
 #endif /* BINARY_TREE_H */
