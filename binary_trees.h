57ccab6d69afe75e3df64cec801555d19f7153e2
Nachop51 <nachoperalta0@gmail.com>
2022-07-18 04:55:38 +0000

    missing header file again

--- binary_trees.h
+++ binary_trees.h
@@ -58,5 +58,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif /* BINARY_TREE_H */
